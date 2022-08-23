96d6eb4c7c423fb8f1c7c00fa20b41d7479b96d2
Arslan Zaki <59029560+arslanzaki@users.noreply.github.com>
2022-08-23 18:37:27 +0500

    SearchBar Complete

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,11 +1,32 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { Text, View, TextInput, SafeAreaView } from "react-native";
+import { SearchIcon } from "react-native-heroicons/outline";
+import { useNavigation } from "@react-navigation/native";
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  });
   return (
-    <View className="bg-gray-200 h-12 rounded-lg mx-4">
-      <Text className="font-bold text-center">SearchBar</Text>
-    </View>
+    <SafeAreaView className="bg-gray-200 h-12 rounded-lg mx-4 flex flex-row mt-8 px-4 items-center">
+      <View className="mr-6">
+        <SearchIcon size={35} />
+      </View>
+      <TextInput
+        className="flex-1 h-full text-lg"
+        autoCapitalize="none"
+        autoCorrect={false}
+        placeholder="Search"
+        value={term}
+        onChangeText={onTermChange}
+        // onEndEditing={onTermSubmit}
+        onSubmitEditing={onTermSubmit}
+      />
+      {/* <Text className="font-bold">SearchBar</Text> */}
+    </SafeAreaView>
   );
 };
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, View } from "react-native";
 import SearchBar from "../components/SearchBar";
 
 const HomeScreen = () => {
+  const [term, setTerm] = useState("");
   return (
     <View>
-      <SearchBar />
+      <SearchBar
+        term={term}
+        onTermChange={(newTerm) => setTerm(newTerm)}
+        onTermSubmit={() => console.log("Term Submitted")}
+      />
       <Text className="text-green-500 text-2xl font-bold text-center">
         HomeScreen
       </Text>
+      <Text>{term}</Text>
     </View>
   );
 };
